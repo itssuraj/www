7aac753e2b62b48df9864f071314c98052371398
Oguzhan Ozdemir <aoozdemir@live.com>
2017-09-16 20:00:41 +0300

    add Q&As to the FAQ. adjust styles.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $(function() {
+  $('a[href^="#"]:not([href="#"])').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top - 50
+    }, 750, 'swing');
+  });
+
   $.getJSON("./js/faq.json", function(json) {
     var q;
     var a;
@@ -7,7 +18,7 @@ $(function() {
       q = "<span class='qtitle'>" + value.q + "</span>";
       a = "<p class='qdesc'>" + value.a + "</p>";
 
-      $("#faq ul").append("<li>" + q + a + "</li>");
+      $("#faq .questions ul").append("<li>" + q + a + "</li>");
     });
   });
 });
